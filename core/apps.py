e8e25aa8b76743ebaae651588334d734a55deff0
Gabriel Jean <29616662+GabrielJean@users.noreply.github.com>
2026-03-03 14:50:24 -0500

    feat: customize Django admin site branding in CoreConfig

--- core/apps.py
+++ core/apps.py
@@ -5,3 +5,10 @@ class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
     label = "core"
+
+    def ready(self):
+        """Customize Django admin site branding."""
+        from django.contrib import admin
+        admin.site.site_header = "FinGlass Administration"
+        admin.site.site_title = "FinGlass Admin"
+        admin.site.index_title = "Welcome to FinGlass Admin"
